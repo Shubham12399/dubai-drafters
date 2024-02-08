7965bcb85b2a557d195749f60c4e8b3101cf60c5
shubham sharma <wifibri2@gmail.com>
2024-02-08 10:57:35 +0530

    logo size

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
     <div className="w-full bg-white shadow-sm relative z-[9999] md:sticky md:top-0">
         <nav className="max-w-maxContent mx-auto py-2 md:py-2 bg-white px-4 flex items-center justify-between ">
            <Link to={"/"} className="w-fit select-none">
-            <img src={logo} alt="company_logo" className="block w-[110px] sm:w[125px] md:w-[160px]" />
+            <img src={logo} alt="company_logo" className="block w-[130px] sm:w[125px] md:w-[160px]" />
            </Link>
            <div className="flex gap-x-3 items-center">
 
