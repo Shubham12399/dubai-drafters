04c840c0a9f4f74f5df75979250d88c8e92a61d4
shubham sharma <wifibri2@gmail.com>
2024-04-11 13:42:52 +0530

    lazy commit

--- src/components/core/home/TypeTours.jsx
+++ src/components/core/home/TypeTours.jsx
@@ -25,13 +25,13 @@ const TypeTours = ({title}) => {
             famous landmarks in top destinations worldwide - your wanderlust
             awaits!
           </p>
-          <div className="mt-3 flex gap-x-2 md:gap-x-4 md:mt-4 overflow-auto hide-scrollbar pb-6 pr-3">
-            {/* <TourCard
+          <div className="mt-3 flex gap-x-3 md:gap-x-4 md:mt-4 overflow-auto hide-scrollbar pb-6 pr-3">
+            <TourCard
               tourTitle={"India Gate Full Day Tour"}
               tourImg="https://res.cloudinary.com/responsivebreakpoints/image/upload/c_scale,w_200/v1710420324/IMG-20240205-WA0063_v4wa9d.jpg"
               tourPrice={355.0}
               starsCount={4}
-            ></TourCard> */}
+            ></TourCard>
             <div className="min-w-[140px] w-[130px] sm:w-[160px] md:min-w-[170px] md:w-[170px] rounded-xl overflow-hidden relative bg-white shadow-sms">
               <div className="w-full">
                 <img
@@ -41,10 +41,10 @@ const TypeTours = ({title}) => {
                   className="w-full h-[110px] sm:h-[110px] object-cover rounded-lg"
                 />
               </div>
-              <div className="px-2 sm:px-4 my-2 mb-3 sm:my-4 ">
+              <div className="px-0 sm:px-4 my-2 mb-3 sm:my-4 ">
                 <Tooltip content={"Evenning Desert Saudi Arabia, late Night"}>
-                  <h3 className="text-[13px] sm:text-sm leading-tight font-medium line-clamp-2 mb-2">
-                    Evening Desert India Gate Full Day Tour.{" "}
+                  <h3 className="text-[13px] sm:text-sm font-medium line-clamp-1">
+                  Evenning Desert Saudi Arabia, late Night{" "}
                   </h3>
                 </Tooltip>
                 <h3 className="text-xs font-medium text-richblack-900 mt-1">
@@ -82,22 +82,22 @@ const TypeTours = ({title}) => {
               <div className="w-full">
                 <img
                   loading="lazy"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYO0vp4oh2c7_tdTJz-pXjIEXQCveDjzxBBHjiNemrjg&s"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0inkbVcTRPSTPLwPJ8Q95UreE7VVW3BQbwK9jlQYgpA&s"
                   alt=""
                   className="w-full h-[110px] sm:h-[110px] object-cover rounded-lg"
                 />
               </div>
-              <div className="px-2 sm:px-4 my-2 mb-3 sm:my-4 ">
-                 <Tooltip content={"Evenning Desert Saudi Arabia, late Night"}>
-                  <h3 className="text-[13px] sm:text-sm leading-tight font-medium line-clamp-2 mb-2">
-                    Evening Desert India Gate Full Day Tour.{" "}
+              <div className="px-0 sm:px-4 my-2 mb-3 sm:my-4 ">
+                <Tooltip content={"Evenning Desert Saudi Arabia, late Night"}>
+                  <h3 className="text-[13px] sm:text-sm font-medium line-clamp-1">
+                  Evenning Desert Saudi Arabia, late Night{" "}
                   </h3>
                 </Tooltip>
                 <h3 className="text-xs font-medium text-richblack-900 mt-1">
                   AED 135.00
                 </h3>
                 <div className="flex items-center justify-between flex-wrap sm:flex-nowrap mt-0 sm:mt-1">
-                  {/* <p className="text-[8px] text-richblack-200 fex items-center mt-1">277 Reviews</p> */}
+                  {/* <p className="text-[8px] text-richblack-200 flex items-center mt-1">277 Reviews</p> */}
                   <div className="flex mt-1 sm:mt-0 gap-x-1 items-center">
                     <FaStar className="text-yellow-100 text-[8px]"></FaStar>
                     <FaStar className="text-yellow-100 text-[8px]"></FaStar>
@@ -107,6 +107,7 @@ const TypeTours = ({title}) => {
                   </div>
                 </div>
                 <div className="flex !items-strech gap-1 mt-3">
+                  <Link to="/tour/tour1">
                   <Button
                     size="sm"
                     variant="outlined"
@@ -114,8 +115,11 @@ const TypeTours = ({title}) => {
                   >
                     Book Now
                   </Button>
+                  </Link>
+
                   <button className="bg-none relative z-20 px-1 pl-2 ml-3 border-none before:w-full before:h-full before:bg-none before:hover:bg-richblack-25 before:absolute before:top-0 before:left-[1px] before:py-2 before:rounded-full before:z-0">
                     <MdOutlineAddShoppingCart className="text-lg relative z-30 text-richblack-500"></MdOutlineAddShoppingCart>
+
                   </button>
                 </div>
               </div>
@@ -124,22 +128,22 @@ const TypeTours = ({title}) => {
               <div className="w-full">
                 <img
                   loading="lazy"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQncMiGb3NbkfCH-w2OTk8UTJwgaCP9zJztyp8eQDipkQ&s"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0inkbVcTRPSTPLwPJ8Q95UreE7VVW3BQbwK9jlQYgpA&s"
                   alt=""
                   className="w-full h-[110px] sm:h-[110px] object-cover rounded-lg"
                 />
               </div>
-              <div className="px-2 sm:px-4 my-2 mb-3 sm:my-4 ">
-                 <Tooltip content={"Evenning Desert Saudi Arabia, late Night"}>
-                  <h3 className="text-[13px] sm:text-sm leading-tight font-medium line-clamp-2 mb-2">
-                    Evening Desert India Gate Full Day Tour.{" "}
+              <div className="px-0 sm:px-4 my-2 mb-3 sm:my-4 ">
+                <Tooltip content={"Evenning Desert Saudi Arabia, late Night"}>
+                  <h3 className="text-[13px] sm:text-sm font-medium line-clamp-1">
+                  Evenning Desert Saudi Arabia, late Night{" "}
                   </h3>
                 </Tooltip>
                 <h3 className="text-xs font-medium text-richblack-900 mt-1">
                   AED 135.00
                 </h3>
                 <div className="flex items-center justify-between flex-wrap sm:flex-nowrap mt-0 sm:mt-1">
-                  {/* <p className="text-[8px] text-richblack-200 flex items-center mt-1">277 Reviews</p> */}{" "}
+                  {/* <p className="text-[8px] text-richblack-200 flex items-center mt-1">277 Reviews</p> */}
                   <div className="flex mt-1 sm:mt-0 gap-x-1 items-center">
                     <FaStar className="text-yellow-100 text-[8px]"></FaStar>
                     <FaStar className="text-yellow-100 text-[8px]"></FaStar>
@@ -149,6 +153,7 @@ const TypeTours = ({title}) => {
                   </div>
                 </div>
                 <div className="flex !items-strech gap-1 mt-3">
+                  <Link to="/tour/tour1">
                   <Button
                     size="sm"
                     variant="outlined"
@@ -156,8 +161,11 @@ const TypeTours = ({title}) => {
                   >
                     Book Now
                   </Button>
+                  </Link>
+
                   <button className="bg-none relative z-20 px-1 pl-2 ml-3 border-none before:w-full before:h-full before:bg-none before:hover:bg-richblack-25 before:absolute before:top-0 before:left-[1px] before:py-2 before:rounded-full before:z-0">
                     <MdOutlineAddShoppingCart className="text-lg relative z-30 text-richblack-500"></MdOutlineAddShoppingCart>
+
                   </button>
                 </div>
               </div>
@@ -166,22 +174,22 @@ const TypeTours = ({title}) => {
               <div className="w-full">
                 <img
                   loading="lazy"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaPKuwHS1SlaIozbG9MtSq8nKN88GqIFntTsaeRql0Rw&s"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0inkbVcTRPSTPLwPJ8Q95UreE7VVW3BQbwK9jlQYgpA&s"
                   alt=""
                   className="w-full h-[110px] sm:h-[110px] object-cover rounded-lg"
                 />
               </div>
-              <div className="px-2 sm:px-4 my-2 mb-3 sm:my-4 ">
-                 <Tooltip content={"Evenning Desert Saudi Arabia, late Night"}>
-                  <h3 className="text-[13px] sm:text-sm leading-tight font-medium line-clamp-2 mb-2">
-                    Evening Desert India Gate Full Day Tour.{" "}
+              <div className="px-0 sm:px-4 my-2 mb-3 sm:my-4 ">
+                <Tooltip content={"Evenning Desert Saudi Arabia, late Night"}>
+                  <h3 className="text-[13px] sm:text-sm font-medium line-clamp-1">
+                  Evenning Desert Saudi Arabia, late Night{" "}
                   </h3>
                 </Tooltip>
                 <h3 className="text-xs font-medium text-richblack-900 mt-1">
                   AED 135.00
                 </h3>
                 <div className="flex items-center justify-between flex-wrap sm:flex-nowrap mt-0 sm:mt-1">
-                  {/* <p className="text-[8px] text-richblack-200 flex items-center mt-1">277 Reviews</p> */}{" "}
+                  {/* <p className="text-[8px] text-richblack-200 flex items-center mt-1">277 Reviews</p> */}
                   <div className="flex mt-1 sm:mt-0 gap-x-1 items-center">
                     <FaStar className="text-yellow-100 text-[8px]"></FaStar>
                     <FaStar className="text-yellow-100 text-[8px]"></FaStar>
@@ -191,6 +199,7 @@ const TypeTours = ({title}) => {
                   </div>
                 </div>
                 <div className="flex !items-strech gap-1 mt-3">
+                  <Link to="/tour/tour1">
                   <Button
                     size="sm"
                     variant="outlined"
@@ -198,8 +207,11 @@ const TypeTours = ({title}) => {
                   >
                     Book Now
                   </Button>
+                  </Link>
+
                   <button className="bg-none relative z-20 px-1 pl-2 ml-3 border-none before:w-full before:h-full before:bg-none before:hover:bg-richblack-25 before:absolute before:top-0 before:left-[1px] before:py-2 before:rounded-full before:z-0">
                     <MdOutlineAddShoppingCart className="text-lg relative z-30 text-richblack-500"></MdOutlineAddShoppingCart>
+
                   </button>
                 </div>
               </div>
@@ -208,22 +220,22 @@ const TypeTours = ({title}) => {
               <div className="w-full">
                 <img
                   loading="lazy"
-                  src="https://www.avanse.com/blogs/images/blog-4-aug.jpg"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0inkbVcTRPSTPLwPJ8Q95UreE7VVW3BQbwK9jlQYgpA&s"
                   alt=""
                   className="w-full h-[110px] sm:h-[110px] object-cover rounded-lg"
                 />
               </div>
-              <div className="px-2 sm:px-4 my-2 mb-3 sm:my-4 ">
-                 <Tooltip content={"Evenning Desert Saudi Arabia, late Night"}>
-                  <h3 className="text-[13px] sm:text-sm leading-tight font-medium line-clamp-2 mb-2">
-                    Evening Desert India Gate Full Day Tour.{" "}
+              <div className="px-0 sm:px-4 my-2 mb-3 sm:my-4 ">
+                <Tooltip content={"Evenning Desert Saudi Arabia, late Night"}>
+                  <h3 className="text-[13px] sm:text-sm font-medium line-clamp-1">
+                  Evenning Desert Saudi Arabia, late Night{" "}
                   </h3>
                 </Tooltip>
                 <h3 className="text-xs font-medium text-richblack-900 mt-1">
                   AED 135.00
                 </h3>
                 <div className="flex items-center justify-between flex-wrap sm:flex-nowrap mt-0 sm:mt-1">
-                  {/* <p className="text-[8px] text-richblack-200 flex items-center mt-1">277 Reviews</p> */}{" "}
+                  {/* <p className="text-[8px] text-richblack-200 flex items-center mt-1">277 Reviews</p> */}
                   <div className="flex mt-1 sm:mt-0 gap-x-1 items-center">
                     <FaStar className="text-yellow-100 text-[8px]"></FaStar>
                     <FaStar className="text-yellow-100 text-[8px]"></FaStar>
@@ -233,6 +245,7 @@ const TypeTours = ({title}) => {
                   </div>
                 </div>
                 <div className="flex !items-strech gap-1 mt-3">
+                  <Link to="/tour/tour1">
                   <Button
                     size="sm"
                     variant="outlined"
@@ -240,12 +253,62 @@ const TypeTours = ({title}) => {
                   >
                     Book Now
                   </Button>
+                  </Link>
+
                   <button className="bg-none relative z-20 px-1 pl-2 ml-3 border-none before:w-full before:h-full before:bg-none before:hover:bg-richblack-25 before:absolute before:top-0 before:left-[1px] before:py-2 before:rounded-full before:z-0">
                     <MdOutlineAddShoppingCart className="text-lg relative z-30 text-richblack-500"></MdOutlineAddShoppingCart>
+
                   </button>
                 </div>
               </div>
             </div>
+            <div className="min-w-[140px] w-[130px] sm:w-[160px] md:min-w-[170px] md:w-[170px] rounded-xl overflow-hidden relative bg-white shadow-sms">
+              <div className="w-full">
+                <img
+                  loading="lazy"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0inkbVcTRPSTPLwPJ8Q95UreE7VVW3BQbwK9jlQYgpA&s"
+                  alt=""
+                  className="w-full h-[110px] sm:h-[110px] object-cover rounded-lg"
+                />
+              </div>
+              <div className="px-0 sm:px-4 my-2 mb-3 sm:my-4 ">
+                <Tooltip content={"Evenning Desert Saudi Arabia, late Night"}>
+                  <h3 className="text-[13px] sm:text-sm font-medium line-clamp-1">
+                  Evenning Desert Saudi Arabia, late Night{" "}
+                  </h3>
+                </Tooltip>
+                <h3 className="text-xs font-medium text-richblack-900 mt-1">
+                  AED 135.00
+                </h3>
+                <div className="flex items-center justify-between flex-wrap sm:flex-nowrap mt-0 sm:mt-1">
+                  {/* <p className="text-[8px] text-richblack-200 flex items-center mt-1">277 Reviews</p> */}
+                  <div className="flex mt-1 sm:mt-0 gap-x-1 items-center">
+                    <FaStar className="text-yellow-100 text-[8px]"></FaStar>
+                    <FaStar className="text-yellow-100 text-[8px]"></FaStar>
+                    <FaStar className="text-yellow-100 text-[8px]"></FaStar>
+                    <FaStar className="text-yellow-100 text-[8px]"></FaStar>
+                    <FaStar className="text-yellow-100 text-[8px]"></FaStar>
+                  </div>
+                </div>
+                <div className="flex !items-strech gap-1 mt-3">
+                  <Link to="/tour/tour1">
+                  <Button
+                    size="sm"
+                    variant="outlined"
+                    className="normal-case py-[.4rem] rounded-md w-[70px] px-2 text-[10px] sm:text-[10px] hover:bg-[#ff621c] hover:text-white text-[#ff612c] border-[#ff612c] font-normal"
+                  >
+                    Book Now
+                  </Button>
+                  </Link>
+
+                  <button className="bg-none relative z-20 px-1 pl-2 ml-3 border-none before:w-full before:h-full before:bg-none before:hover:bg-richblack-25 before:absolute before:top-0 before:left-[1px] before:py-2 before:rounded-full before:z-0">
+                    <MdOutlineAddShoppingCart className="text-lg relative z-30 text-richblack-500"></MdOutlineAddShoppingCart>
+
+                  </button>
+                </div>
+              </div>
+            </div>
+         
           </div>
         </div>
     </>
